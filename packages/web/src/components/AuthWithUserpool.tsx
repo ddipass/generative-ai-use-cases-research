84bd5ba464c40c178d11b3f954fff046786c198d
ddipass <ddipass@gmail.com>
2024-07-05 22:34:17 +0800

    Login

--- packages/web/src/components/AuthWithUserpool.tsx
+++ packages/web/src/components/AuthWithUserpool.tsx
@@ -30,10 +30,87 @@ const AuthWithUserpool: React.FC = () => {
             Generative AI for Research on AWS
           </div>
         ),
-      }}>
+      }}>   
       <App />
     </Authenticator>
   );
 };
 
 export default AuthWithUserpool;
+
+
+
+
+
+
+import { Amplify } from 'aws-amplify';
+import { Authenticator, translations, CheckboxField, useAuthenticator } from '@aws-amplify/ui-react';
+import App from '../App.tsx';
+import { I18n } from 'aws-amplify/utils';
+
+const selfSignUpEnabled: boolean =
+  import.meta.env.VITE_APP_SELF_SIGN_UP_ENABLED === 'true';
+
+const AuthWithUserpool: React.FC = () => {
+  Amplify.configure({
+    Auth: {
+      Cognito: {
+        userPoolId: import.meta.env.VITE_APP_USER_POOL_ID,
+        userPoolClientId: import.meta.env.VITE_APP_USER_POOL_CLIENT_ID,
+        identityPoolId: import.meta.env.VITE_APP_IDENTITY_POOL_ID,
+      },
+    },
+  });
+
+  I18n.putVocabularies(translations);
+  I18n.setLanguage('en');
+  // I18n.setLanguage('ja');
+
+  return (
+    <Authenticator
+      hideSignUp={!selfSignUpEnabled}
+      initialState="signUp"
+      components={{
+        Header: () => (
+          <div className="text-aws-font-color mb-5 mt-10 flex justify-center text-3xl">
+            Generative AI for Research on AWS
+          </div>
+        ),
+        SignUp: {
+          FormFields() {
+            const { validationErrors } = useAuthenticator();
+
+            return (
+              <>
+                <Authenticator.SignUp.FormFields />
+                <CheckboxField
+                  errorMessage={validationErrors.acknowledgement as string}
+                  hasError={!!validationErrors.acknowledgement}
+                  name="acknowledgement"
+                  value="yes"
+                  label="I agree with the Terms and Conditions"
+                />
+              </>
+            );
+          },
+        },
+      }}
+      services={{
+        async validateCustomSignUp(formData) {
+          if (!formData.acknowledgement) {
+            return {
+              acknowledgement: 'You must agree to the Terms and Conditions',
+            };
+          }
+        },
+      }}
+    >
+    <App />
+    </Authenticator>
+  );
+};
+
+export default AuthWithUserpool;
+
+
+
