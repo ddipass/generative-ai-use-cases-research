01fa19e565750682c568c18be74e02ee0aaa2d11
ddipass <ddipass@gmail.com>
2024-07-05 22:05:56 +0800

    Init

--- packages/web/src/components/AuthWithUserpool.tsx
+++ packages/web/src/components/AuthWithUserpool.tsx
@@ -1,7 +1,6 @@
 import { Amplify } from 'aws-amplify';
 import { Authenticator, translations } from '@aws-amplify/ui-react';
 import App from '../App.tsx';
-import CustomSignIn from './CustomSignIn.tsx';
 import { I18n } from 'aws-amplify/utils';
 
 const selfSignUpEnabled: boolean =
@@ -31,24 +30,10 @@ const AuthWithUserpool: React.FC = () => {
             Generative AI for Research on AWS
           </div>
         ),
-        SignIn: CustomSignIn,  // 使用自定义的 SignIn 组件
       }}>
       <App />
     </Authenticator>
   );
-  // return (
-  //   <Authenticator
-  //     hideSignUp={!selfSignUpEnabled}
-  //     components={{
-  //       Header: () => (
-  //         <div className="text-aws-font-color mb-5 mt-10 flex justify-center text-3xl">
-  //           Generative AI for Research on AWS
-  //         </div>
-  //       ),
-  //     }}>
-  //     <App />
-  //   </Authenticator>
-  // );
 };
 
 export default AuthWithUserpool;
